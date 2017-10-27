75a15e501327a45f35f4b18261d16b06805efd94
kwonjae lee <kwonjae.2@navercorp.com>
2017-10-27 23:47:50 +0900

    set data

--- graph_classifier/__init__.py
+++ graph_classifier/__init__.py
@@ -1,9 +1,21 @@
 import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-data = {}
+json_data = {}
+# csv_data = pd.read_csv('cpu_usage2.csv')
 
-with open("./data", 'r') as file:
-    data = file.read()
-    data = json.loads(data)
+with open('./data', 'r') as file:
+    json_data = file.read()
+    json_data = json.loads(json_data)
 
-print(data.get('data1'))
+# print("======cpu_usage data======")
+# print("file length : " + str(len(csv_data['value'])))
+
+plt.figure(figsize=[100,3])
+plt.subplot(2,1,1)
+plt.plot(json_data['value1'])
+plt.subplot(2,1,2)
+plt.plot(json_data['value2'])
+plt.show()
